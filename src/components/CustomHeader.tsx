3cd7aa712d75807a7640bc4c3a9eef93b42e8453
Nicholas <quackshows11@gmail.com>
2025-03-04 21:49:19 +0100

    fix: Changed the tab names in the header

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,6 +1,6 @@
 export default function CustomHeader() {
   return (
-    <div className="bg-[#ecece7] py-3">
+    <div className="bg-[#ecece7] py-3 rounded-2xl">
       <div className="flex flex-row justify-between py-2">
         <div className="flex flex-row  items-end gap-2">
           <p className="font-bold  pl-4">Nicholas Nieminen Jönsson</p>
@@ -9,9 +9,10 @@ export default function CustomHeader() {
 
         <div className="flex flex-row  mr-4 items-end">
           <p className="px-4 border-black border-r">Resume </p>
-          <p className="px-4 border-black border-r">Research</p>
-          <p className="px-4 border-black border-r">Outreach</p>
-          <p className="pl-4 border-black ">Personal</p>
+          <p className="px-4 border-black border-r">Portfolio</p>
+          <p className="px-4 border-black border-r">About Me</p>
+          <p className="px-4 border-black border-r">Skills</p>
+          <p className="pl-4 border-black ">Contact</p>
         </div>
       </div>
     </div>
