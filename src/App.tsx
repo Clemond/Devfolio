4a34f73406f431d6588b4f5396963cdbe184eb78
Nicholas <quackshows11@gmail.com>
2025-03-05 14:43:35 +0100

    feature: created a AboutMeCard component and fixed some flex issues in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import AboutMeCard from "./components/aboutMe/AboutMeCard";
 import CustomHeader from "./components/CustomHeader";
 import ProfileIntroCard from "./components/ProfileIntroCard";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen bg-[#0f1021]">
-      <CustomHeader />
-      <div className="flex flex-1 justify-center items-center ">
-        <ProfileIntroCard />
+    <div className="flex flex-col">
+      <div className="h-screen flex flex-col ">
+        <CustomHeader />
+        <div className="flex flex-grow justify-center items-center">
+          <ProfileIntroCard />
+        </div>
       </div>
+
+      <AboutMeCard />
     </div>
   );
 }
--- src/components/ProfileIntroCard.tsx
+++ src/components/ProfileIntroCard.tsx
@@ -3,16 +3,15 @@ import SocialMediaLinks from "./SocialMediaLinks";
 
 export default function ProfileIntroCard() {
   return (
-    <div className="w-full justify-center flex ">
-      <div className=" flex flex-row w-[70%] ">
+    <div className="w-full flex flex-col justify-center items-center flex-1">
+      <div className="flex flex-row w-[70%] justify-center">
         <img
           className="rounded-[192px] h-96 w-96 shadow-2xl shadow-[#c227de]"
           src={ProfilePic}
           alt="Profile Pic"
         />
-
-        <div className=" text-center pl-10 flex flex-col justify-center gap-3 ">
-          <p className=" text-[20px] text-gray-300">Hello, i'm</p>
+        <div className="text-center pl-10 flex flex-col justify-center gap-3">
+          <p className="text-[20px] text-gray-300">Hello, I'm</p>
           <p className="font-bold text-[35px]">Nicholas Nieminen Jönsson</p>
           <p className="italic text-[20px] text-gray-300">
             Application Developer Student
@@ -25,7 +24,7 @@ export default function ProfileIntroCard() {
               Download CV
             </button>
             <button
-              className="bg-white text-black  rounded-3xl opacity-80 hover:opacity-100 active:opacity-60 transition-opacity duration-200 w-36"
+              className="bg-white text-black rounded-3xl opacity-80 hover:opacity-100 active:opacity-60 transition-opacity duration-200 w-36"
               onClick={() => alert("TBD")}
             >
               Contact Info
